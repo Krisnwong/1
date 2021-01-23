26729c614a1a870eaf754234e30324d1f3556644
Krisnwong <Kristen.ka.212@gmail.com>
2021-01-23 19:02:59 +0000

    input learn

--- main.py
+++ main.py
@@ -1,2 +1,30 @@
+import time
+
+# Assignment and print
+
 foo = "Kristen"
 bar = 1
+print("hello world")
+
+# Input
+
+name = input("What is your name?")
+print("Hello " + name)
+time.sleep(1)
+print("Let's do adding numbers.")
+time.sleep(1)
+num1 = input('number one')
+num1 = int(num1)  # This is somewhat, for now, important
+num2 = input('number two')
+num2 = int(num2)
+time.sleep(1)
+print("...")
+time.sleep(1)
+print("drum roll")
+time.sleep(1)
+num3 = sum([num1, num2])
+print(num3)
+time.sleep(1)
+print("TADAA")
+
+# Conditionals
