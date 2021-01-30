423bff412d2e8ee7c194bdf58858fa73d91c1d6a
Krisnwong <Kristen.ka.212@gmail.com>
2021-01-30 18:43:11 +0000

    conditionals

--- main.py
+++ main.py
@@ -1,30 +1,39 @@
 import time
 
-# Assignment and print
+# # Assignment and print
 
-foo = "Kristen"
-bar = 1
-print("hello world")
+# foo = "Kristen"
+# bar = 1
+# print("hello world")
 
-# Input
+# # Input
 
-name = input("What is your name?")
-print("Hello " + name)
-time.sleep(1)
-print("Let's do adding numbers.")
-time.sleep(1)
-num1 = input('number one')
-num1 = int(num1)  # This is somewhat, for now, important
-num2 = input('number two')
-num2 = int(num2)
-time.sleep(1)
-print("...")
-time.sleep(1)
-print("drum roll")
-time.sleep(1)
-num3 = sum([num1, num2])
-print(num3)
-time.sleep(1)
-print("TADAA")
+# name = input("What is your name?")
+# print("Hello " + name)
+# time.sleep(1)
+# print("Let's do adding numbers.")
+# time.sleep(1)
+# num1 = input('number one')
+# num1 = int(num1)  # This is somewhat, for now, important
+# num2 = input('number two')
+# num2 = int(num2)
+# time.sleep(1)
+# print("...")
+# time.sleep(1)
+# print("drum roll")
+# time.sleep(1)
+# num3 = sum([num1, num2])
+# print(num3)
+# time.sleep(1)
+# print("TADAA")
 
 # Conditionals
+
+word_length = len(input("give me a word"))
+if word_length > 15:
+  print('this is a really long word')
+elif word_length > 10:
+  print('this is a long word')
+else:
+  print('this is a short word')
+
